6fd6b9df2e2c84712a079825b414ed1a397e6a2c
Ed Eguaikhide <eguaikhidee@gmail.com>
2025-12-16 22:15:15 -0500

    Force apply Terraform formatting

--- infra/apigw.tf
+++ infra/apigw.tf
@@ -4,9 +4,9 @@ resource "aws_apigatewayv2_api" "http_api" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.crud_lambda.invoke_arn
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.crud_lambda.invoke_arn
   payload_format_version = "2.0"
 }
 
